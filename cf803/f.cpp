93a673c143688364c8ed833bd19e8b0d76f7fecf
zjma <yzxz@hotmail.com>
2017-04-30 02:06:31 +0800

    cf803 f done

--- cf803/f.cpp
+++ cf803/f.cpp
@@ -44,13 +44,49 @@ const ll M = 1000000007;
 ll scan() {ll r;scanf("%lld",&r);return r;}
 void scanstr(char *buf){scanf("%s",buf);}
 
+ll mod(ll x, ll m) { return ((x%m) + m) % m; }
+ll add(ll a, ll b, ll m){return mod(mod(a,m)+mod(b,m),m);}
+ll sub(ll a, ll b, ll m){return mod(mod(a,m)-mod(b,m),m);}
+ll mul(ll a, ll b, ll m){return mod(mod(a,m)*mod(b,m),m);}
+
+const ll N=100000;
 int main()
 {
     ll n=scan();
-    VI a(n);
+    VI c(N+1);
     rng(i,0,n){
-        a[i]=scan();
+        ll rd=scan();
+        ++c[rd];
     }
 
+    VI p(N+1);
+    p[0]=1;
+    rng(i,1,N+1){
+        p[i]=mul(p[i-1],2,M);
+    }
+
+    VI g(N+1);
+    rng(i,1,N+1){
+        ll t=0;
+        ll j=i;
+        while(j<=N){
+            t=add(t,c[j],M);
+            j+=i;
+        }
+        g[i]=sub(p[t],1,M);
+    }
+
+    VI f(N+1);
+    ll i=N;
+    while(i>=1){
+        f[i]=g[i];
+        ll j=i*2;
+        while(j<=N){
+            f[i]=sub(f[i],f[j],M);
+            j+=i;
+        }
+        --i;
+    }
+    cout<<f[1]<<endl;
     return 0;
 }
