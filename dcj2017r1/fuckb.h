e828a71a1ee704ad959cd07cbed185d8ebbf9d2e
zjma <yzxz@hotmail.com>
2017-05-15 03:11:10 +0800

    try apply new partition algorithm

--- dcj2017r1/fuckb.h
+++ dcj2017r1/fuckb.h
@@ -2,24 +2,24 @@
 long long GetNumberLength() {return 20LL;}
 long long GetDigit(long long i) {
 switch ((int)i) {
-case 0: return 8LL;
-case 1: return 6LL;
-case 2: return 7LL;
-case 3: return 7LL;
-case 4: return 1LL;
-case 5: return 1LL;
-case 6: return 6LL;
-case 7: return 1LL;
-case 8: return 8LL;
-case 9: return 4LL;
-case 10: return 5LL;
-case 11: return 5LL;
-case 12: return 4LL;
+case 0: return 2LL;
+case 1: return 4LL;
+case 2: return 5LL;
+case 3: return 9LL;
+case 4: return 4LL;
+case 5: return 5LL;
+case 6: return 1LL;
+case 7: return 7LL;
+case 8: return 1LL;
+case 9: return 1LL;
+case 10: return 0LL;
+case 11: return 3LL;
+case 12: return 6LL;
 case 13: return 6LL;
-case 14: return 2LL;
-case 15: return 3LL;
-case 16: return 4LL;
-case 17: return 8LL;
-case 18: return 4LL;
-case 19: return 9LL;
+case 14: return 8LL;
+case 15: return 9LL;
+case 16: return 9LL;
+case 17: return 9LL;
+case 18: return 0LL;
+case 19: return 8LL;
 default: assert(0);}}
