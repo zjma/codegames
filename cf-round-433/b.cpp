5c64014ae6895b82c6c8e34c78a7904efc1fc881
zjma <yzxz@hotmail.com>
2017-09-07 20:42:25 +0800

    cf4433b

--- cf-round-433/b.cpp
+++ cf-round-433/b.cpp
@@ -44,8 +44,80 @@ const ll M = 1000000007;
 ll scan() {ll r;scanf("%lld",&r);return r;}
 void scanstr(char *buf){scanf("%s",buf);}
 
+const ll MAXDAY=1000000;
+const ll MAXCITY=100000;
 int main()
 {
-    cout<<"WORKS"<<endl;
+    ll n=scan();
+    ll m=scan();
+    ll k=scan();
+    vector<vector<pair<ll,ll>>> day2gotkts(MAXDAY+1);
+    vector<vector<pair<ll,ll>>> day2leavetkts(MAXDAY+1);
+    rng(i,0,m){
+        ll d=scan();
+        ll f=scan();
+        ll t=scan();
+        ll c=scan();
+        if (f==0){
+            day2leavetkts[d].emplace_back(t,c);
+        }else{
+            day2gotkts[d].emplace_back(f,c);
+        }
+    }
+
+    vector<bool> allablego(MAXDAY+1);
+    VI allgocost(MAXDAY+1);
+    VI city2gocost(MAXCITY+1,INF);
+    ll goableCount=0;
+    ll tmpallgocost=0;
+    rng(i,1,MAXDAY+1){
+        allablego[i]=(goableCount==n);
+        allgocost[i]=tmpallgocost;
+        for (auto pr:day2gotkts[i]){
+            ll hcity,cost;tie(hcity,cost)=pr;
+            if (city2gocost[hcity]==INF){
+                ++goableCount;
+                city2gocost[hcity]=cost;
+                tmpallgocost+=cost;
+            }else{
+                tmpallgocost-=city2gocost[hcity];
+                city2gocost[hcity]=min(city2gocost[hcity],cost);
+                tmpallgocost+=city2gocost[hcity];
+            }
+        }
+    }
+
+    vector<bool> allableleave(MAXDAY+1);
+    VI allleavecost(MAXDAY+1);
+    VI city2leavecost(MAXCITY+1,INF);
+    ll leavableCount=0;
+    ll tmpallleavecost=0;
+    for (ll i=MAXDAY;i>=1;--i){
+        allableleave[i]=(leavableCount==n);
+        allleavecost[i]=tmpallleavecost;
+        for (auto pr:day2leavetkts[i]){
+            ll hcity,cost;tie(hcity,cost)=pr;
+            if (city2leavecost[hcity]==INF){
+                ++leavableCount;
+                city2leavecost[hcity]=cost;
+                tmpallleavecost+=cost;
+            }else{
+                tmpallleavecost-=city2leavecost[hcity];
+                city2leavecost[hcity]=min(city2leavecost[hcity],cost);
+                tmpallleavecost+=city2leavecost[hcity];
+            }
+        }
+    }
+
+    ll ans=INF;
+    rng(startday,1,MAXDAY+1){
+        ll endday=startday+k-1;
+        if (endday>MAXDAY) continue;
+        if (!allablego[startday]) continue;
+        if (!allableleave[endday]) continue;
+        ans=min(ans,allgocost[startday]+allleavecost[endday]);
+    }
+    if (ans==INF) ans=-1;
+    cout<<ans<<endl;
     return 0;
 }
